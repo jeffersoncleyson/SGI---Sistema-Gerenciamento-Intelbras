20abd5647dcd92319c6432c45ddfa85c52632565
Aluno do Inatel <aluno@local.inatel.br>
2018-05-16 11:31:22 -0300

    Classe testes

--- src/br/com/intelbras/model/BancoDados.java
+++ src/br/com/intelbras/model/BancoDados.java
@@ -26,16 +26,16 @@ public class BancoDados {
 
     public BancoDados() {
 
-        this.usuario = "root";
-        this.senha = "root";
-        this.nomeBanco = "intelbras";
-        this.url = "jdbc:mysql://localhost:3306/" + this.nomeBanco;
-        this.JDBC = "com.mysql.jdbc.Driver"; 
+//        this.usuario = "root";
+//        this.senha = "root";
+//        this.nomeBanco = "intelbras";
+//        this.url = "jdbc:mysql://localhost:3306/" + this.nomeBanco;
+//        this.JDBC = "com.mysql.jdbc.Driver"; 
        
-//        this.url ="jdbc:mysql://sql132.main-hosting.eu:3306/u483572784_intbr";
-//        this.JDBC = "com.mysql.jdbc.Driver";
-//        this.usuario = "u483572784_intbr";
-//        this.senha = "u_Root#2018";
+        this.url ="jdbc:mysql://sql132.main-hosting.eu:3306/u483572784_intbr";
+        this.JDBC = "com.mysql.jdbc.Driver";
+        this.usuario = "u483572784_intbr";
+        this.senha = "u_Root#2018";
         
     }
 
